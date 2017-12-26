68c2927cfb9a0310f0423e2cd959df918cca7b94
RamonPaiva <34141267+RamonPaiva@users.noreply.github.com>
2017-12-26 20:51:43 -0200

    Teste com operadores

--- OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -16,9 +16,16 @@ public class OperadoresAritmeticos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int n1=3,n2=5;
+        /*int n1=3,n2=5;
         float m = (n1+n2)/2;
-        System.out.println("A media e: "+m);
+        System.out.println("A media e: "+m);*/
+        
+        /*int numero = 5;
+        numero++;
+        System.out.println(numero);*/
+        double ale = Math.random();
+        int n = (int) (5 + ale * (10-5));
+        System.out.println(n);
     }
     
 }
