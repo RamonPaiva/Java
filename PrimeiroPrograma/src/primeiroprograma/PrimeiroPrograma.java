c4ed1c820f77e36b8687ec85cd684ed4b2e05956
RamonPaiva <34141267+RamonPaiva@users.noreply.github.com>
2017-12-26 14:57:57 -0200

    Inserção no repositório remoto

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,7 @@
+package primeiroprograma;
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        System.out.print("Olá, Mundo");
+        System.out.println("Olá Mundo 2!");
+    }
+}
