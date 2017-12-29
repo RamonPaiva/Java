c546401812edbe341007b53b7207d41eb795c37f
RamonPaiva <34141267+RamonPaiva@users.noreply.github.com>
2017-12-28 22:43:37 -0200

    Declaração de vetor, lenght

---
+++ Vetor01/src/vetor01/Vetor01.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vetor01;
+
+/**
+ *
+ * @author ramon
+ */
+public class Vetor01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //int[] n = new int[4];
+        //int n[] = new int[4];
+        int n[] = {3,8,1,0,7,81};
+        System.out.println(n.length);
+        for(int i=0;i<n.length;i++)
+            System.out.print(n[i] + " ");
+    }
+    
+}
